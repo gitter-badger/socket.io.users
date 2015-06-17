c8038e399c2b8646a03fdc8f52fbc87730c4a8e3
Makis Maropoulos <makis@ideopod.com>
2015-06-17 10:14:46 +0300

    Attach - Detach -Join room - Leave room to SocketUser
    SocketUser split from SocketUsers, implement
    attach method, detach user and join - leave room.
    Also npm package updated to 1.0.1 .

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var Server = require('./lib/socket.server')
-  , Session = require('./lib/socket.session')
-  , Users = require('./lib/socket.users');
+var Server = require('./lib/socket.server'),
+    Session = require('./lib/socket.session'),
+    User = require('./lib/socket.user'),
+    Users = require('./lib/socket.users');
 
 module.exports.Server = Server;
 module.exports.Session = Session;
+module.exports.User = User;
 module.exports.Users = Users;
